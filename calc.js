56340a4918bfda3dbf350c33eb11a4fd01031517
Suleiman_Kuliev <getto_tawers@mail.ru>
2023-01-25 16:48:19 +0400

    мой вариант

--- calc.js
+++ calc.js
@@ -2,13 +2,28 @@ let a = "";
 let b = "";
 let sign = ""; // знак
 let finish = false;
+let calculation = '';
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '( )'];
 const action = ['-', '+', 'X', '/', '+/-'];
-
+const variablesToCalculate = [a, b]
 // экран
 
 const out = document.querySelector('.calc-screen span');
+
+const actionSign = [summation(), ]
+
+function summation(value1, value2) {
+    calculation = Number(value1) + Number(value2);
+    a = calculation;
+    console.log(calculation)
+}
+
+function subtraction (value1, value2) {
+    calculation = value1 - value2;
+    a = calculation;
+    console.log(calculation)
+}
 
 function clearAll () {
     a = '';
@@ -25,9 +40,8 @@ function signChange () {
         out.textContent = a + sign + b;
     }
     console.log(a)
-    finish = true;
     out.textContent = a;
-    out.textContent = a;
+    out.textContent = b;
 }
 
 
@@ -35,66 +49,107 @@ document.querySelector('.plus-minus').onclick = signChange;
 
 document.querySelector('.ac').onclick = clearAll;
 
-document.querySelector('.bracket').onclick = bracket;
-
 document.querySelector('.buttons').onclick = (event) => {
-    if(!event.target.classList.contains('btn')) return; // contains невразумив
-    if(event.target.classList.contains('ac')) return;
+    if (!event.target.classList.contains('btn')) return; // contains невразумив
+    if (event.target.classList.contains('ac')) return;
 
     out.textContent = '';
     const key = event.target.textContent;
 
-
     if (digit.includes(key)) {
         if (b === '' && sign === '') {
             a += key;
-            console.log(a, b, sign);
             out.textContent = a;
-        } else if (a!=='' && b!=='' && finish) {
+            console.log(a)
+        }
+        else if (a!=='' && b!=='' && finish) {
             b = key;
             finish = false;
             out.textContent = a+sign+b;
-        }  else {
-            b += key;
-            out.textContent = a+sign+b;
         }
-        console.log(a, b, sign)
-        return;
+        if (a !== '' && sign !== '') {
+            b += key;
+            out.textContent = a + sign + b;
+            console.log(a, sign, b);
+        }
+        // if (a !== '' && sign !== '' && b !== '') {
+        //     for ( )
+        // }
     }
 
     if (action.includes(key)) {
         sign = key;
-        out.textContent = a+sign;
-        console.log(a, b, sign)
-        console.log('wadasa')
-        return;
+        out.textContent = a + sign;
+        console.log(a, sign, b)
+
     }
 
+
+//     if (digit.includes(key)) {
+//         if (b === '' && sign === '') {
+//             a += key;
+//             console.log(a, b, sign);
+//             out.textContent = a;
+//         } else if (a!=='' && b!=='' && finish) {
+//             b = key;
+//             finish = false;
+//             out.textContent = a+sign+b;
+//         }  else {
+//             b += key;
+//             out.textContent = a+sign+b;
+//         }
+//         console.log(a, b, sign)
+//         return;
+//     }
+//
+//     if (action.includes(key)) {
+//         sign = key;
+//         out.textContent = a+sign;
+//         console.log(a, b, sign)
+//         console.log('wadasa')
+//         return;
+//     }
+//
 // нажато =
-    if (key === '=') {
-        if (b === '') b = a;
-        switch (sign) {
-            case "+":
-                a = Number(a) + Number(b);
-                break;
-            case "/":
-                if (b === '0') {
-                    clearAll();
-                    out.textContent = '∞'
-                    return;
-                }
-                a = a / b;
-                break;
-            case "X":
-                a = a * b;
-                break;
-            case "-":
-                a = a - b;
-                break;
+        if (key === '=') {
+            if (b === '') b = a;
+            switch (sign){
+                case "+":
+                    summation(a, b);
+                    break;
+                case "-":
+                    subtraction(variablesToCalculate)
+                    break;
+            }
+            b = '';
+            finish = true;
+            out.textContent = calculation;
         }
-        finish = true;
-        out.textContent = a;
-        console.log(a, b, sign)
-    }
-}
+    // if (key === '=') {
+    //     if (b === '') b = a;
+    //     switch (sign) {
+    //         case "+":
+    //             a = Number(a) + Number(b);
+    //             break;
+    //         case "/":
+    //             if (b === '0') {
+    //                 clearAll();
+    //                 out.textContent = '∞'
+    //                 return;
+    //             }
+    //             a = a / b;
+    //             break;
+    //         case "X":
+    //             a = a * b;
+    //             break;
+    //         case "-":
+    //             a = a - b;
+    //             break;
+    //     }
+    //     b = '';
+    //     finish = true;
+    //     out.textContent = a;
+    //     console.log(a, b, sign)
+    // }
 
+}
