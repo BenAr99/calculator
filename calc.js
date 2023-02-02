f0d852b27dc0fdb1d8effead83d9b0200947585c
Suleiman_Kuliev <getto_tawers@mail.ru>
2023-02-02 17:54:35 +0400

    Реализована функция erase

--- calc.js
+++ calc.js
@@ -13,7 +13,7 @@ const historyOutput = [];
 let closeScreenHistory = document.querySelector('.btn-history');
 let historyOpen = false;
 
-function openHistory () {
+function openHistory () { // сделать Event
     const screenHistory = document.createElement('ul')
     if (historyOpen === false) {
         historyOpen = true;
@@ -21,21 +21,52 @@ function openHistory () {
         document.querySelector('.calc-screen span').remove()
         screenHistory.className = "history-screen";
         for (let i = 0; i < historyOutput.length; i++) {
-            screenHistory.innerHTML += `<li> ${historyOutput[i]}</li>`
+            screenHistory.innerHTML += `<li>${historyOutput[i]} </li>`
         }
         document.querySelector('.calc-screen').append(screenHistory);
 
+        document.querySelector('.calc-screen ul').onclick = OutputHistoryToTheCalcScreen;
         out.textContent = '';
+        variablesToCalculate = []
         digitBuffer = '';
     } else {
         historyOpen = false;
-
         closeScreenHistory.textContent = 'История'
         document.querySelector('.calc-screen ul').remove()
         const screenSpan = document.createElement('span');
         document.querySelector('.calc-screen').append(screenSpan);
         out = screenSpan;
-        console.log(out)
+    }
+}
+
+function OutputHistoryToTheCalcScreen (e) {
+    if (!(e.target.tagName === 'LI')) return;
+    openHistory();
+    let historyCalculation = e.target;
+    let test2 = '=';
+    // historyCalculation.textContent.split(test2).splice(0, 1);
+    historyCalculation.textContent = [historyCalculation.textContent.split(test2).splice(0, 1)]; // смотреть на 46
+    console.log(historyCalculation.textContent)
+    variablesToCalculate = historyCalculation.textContent.split(' ');
+    variablesToCalculate.pop()
+    console.log(`fff${variablesToCalculate}`)
+    out.textContent = historyCalculation.textContent.split('=')[0];
+    console.log(out.textContent);
+}
+
+function erase () {
+    console.log(digitBuffer);
+    if (action.includes(variablesToCalculate[variablesToCalculate.length-1]) && digitBuffer === '') {
+        variablesToCalculate.pop()
+        out.textContent  = variablesToCalculate
+    } else if (digitBuffer.length === 1) {
+        out.textContent = variablesToCalculate.join(' ')
+        digitBuffer = ''
+    } else if (digitBuffer.length >= 2 ) {
+        digitBuffer = digitBuffer.slice(0, -1);
+        out.textContent = variablesToCalculate.join(' ') + digitBuffer
+        console.log(variablesToCalculate)
+        console.log(digitBuffer)
     }
 }
 
@@ -47,22 +78,29 @@ function clearAll() {
 
 document.querySelector('.btn-history').onclick = openHistory;
 
+document.querySelector('.delete-button-container img').onclick = erase;
+
 document.querySelector('.ac').onclick = clearAll;
 
 document.querySelector('.buttons').onclick = (event) => {
-    if (!event.target.classList.contains('btn')) return; // contains невразумив
+    if (!event.target.classList.contains('btn')) return;
     if (event.target.classList.contains('ac')) return;
 
     const key = event.target.textContent;
     out.textContent += key;
 
-    if (digit.includes(key)) {
+    if (digit.includes(key)) { // другой способ через split
         digitBuffer += key;
         historyBuffer += `${key} `;
     }
-    if (action.includes(key)) {
+    if (action.includes(key) && digitBuffer !=='') {
         variablesToCalculate.push(digitBuffer);
         variablesToCalculate.push(key);
+        historyBuffer += `${key} `;
+        digitBuffer = '';
+    } else if (action.includes(key)) { //
+        variablesToCalculate.push(key);
+        console.log("После знака", variablesToCalculate)
         historyBuffer += `${key} `;
         digitBuffer = '';
     }
@@ -86,7 +124,7 @@ document.querySelector('.buttons').onclick = (event) => {
                     calculation = value1 / value2
                     break
             }
-            historyOutput.push(`${value1} ${variablesToCalculate[1]} ${value2} = ${calculation}`)
+            historyOutput.push(`${value1} ${variablesToCalculate[1]} ${value2} = ${calculation}`);
             variablesToCalculate.splice(0, 3);
             variablesToCalculate.unshift(calculation.toString());
             console.log('Log after calculate: ', variablesToCalculate)
@@ -96,3 +134,4 @@ document.querySelector('.buttons').onclick = (event) => {
         out.textContent = digitBuffer;
     }
 }
+// криво смотрится после вывода решение
